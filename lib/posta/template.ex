ea48828ede303b5a2c3faf3a733a9c326c788334
Uther Pally <utherpally@protonmail.com>
2025-05-05 18:18:18 +0700

    add helpers

--- lib/posta/template.ex
+++ lib/posta/template.ex
@@ -10,4 +10,24 @@ defmodule Posta.Template do
     attachments: [],
     reply_to: nil
   ]
+
+
+if Code.loaded?(Swoosh.Email) do
+  use Swoosh.Email
+
+  def to_swoosh_email(%__MODULE__{} = template) do
+    email = new()
+    |> subject(template.subject)
+    |> to(template.to)
+    |> from(template.from)
+    |> cc(template.cc)
+    |> bcc(template.bcc)
+    |> text_body(template.text_body)
+    |> html_body(template.html_body)
+    |> reply_to(template.reply_to)
+
+    Enum.reduce(template.attachments, email, fn att, email -> attachment(email, att) end)
+  end
+end
+  
 end
